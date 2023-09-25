29151d28042f8c9f25226c64958840bde5f4f294
rya23 <aaryathakur48@gmail.com>
2023-09-25 23:32:38 +0530

    Initialized better loops

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,6 @@
 
 
 
-
-
-
-
-
 import java.util.Scanner;
 
 class Job {
@@ -34,8 +29,8 @@ class Job {
 
 class solve {
     public int maxdeadline(Job[] arr, int n) {
-        int max = arr[0].deadline;
-        for (int i = 0; i < n; i++) {
+        int max = arr[1].deadline;
+        for (int i = 1; i <= n; i++) {
             if (arr[i].deadline > max) {
                 max = arr[i].deadline;
             }
@@ -55,8 +50,8 @@ public class Main {
         System.out.println(arr[1].profit);
         int i, j;
         Job temp;
-        for (i = 0; i < n - 1; i++) {
-            for (j = 0; j < n - 1 - i; j++) {
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < n - i; j++) {
                 if (arr[j].profit < arr[j + 1].profit) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -80,27 +75,35 @@ public class Main {
                 System.out.println("Enter number of Jobs to Schedule : ");
                 n = sc.nextInt();
                 System.out.println(n);
-                arr = new Job[n];
-                for (int i = 0; i < n; i++) {
+                arr = new Job[n+1];
+                for (int i = 1; i <= n; i++) {
 
-                    System.out.println("Enter Profit and Deadline of Job " + (i + 1) + ": ");
+                    System.out.println("Enter Profit and Deadline of Job " + i + ": ");
                     int profit = sc.nextInt();
                     int deadline = sc.nextInt();
-                    arr[i] = new Job(i + 1, profit, deadline);
+                    arr[i] = new Job(i, profit, deadline);
                     arr[i].printdata();
                 }
                 break;
             }
             case 2: {
-                n = 4;
-                arr = new Job[n];
-                arr[0] = new Job(1, 20, 4);
-                arr[1] = new Job(2, 10, 1);
-                arr[2] = new Job(3, 40, 2);
-                arr[3] = new Job(4, 20, 2);
+//                n = 4;
+//                arr = new Job[n+1];
+//                arr[1] = new Job(1, 20, 4);
+//                arr[2] = new Job(2, 10, 1);
+//                arr[3] = new Job(3, 40, 2);
+//                arr[4] = new Job(4, 20, 2);
 
+                n = 6;
+                arr = new Job[n+1];
+                arr[1] = new Job(1, 30, 3);
+                arr[2] = new Job(2, 25, 1);
+                arr[3] = new Job(3, 50, 3);
+                arr[4] = new Job(4, 40, 2);
+                arr[5] = new Job(5, 10, 1);
+                arr[6] = new Job(6, 35, 4);
+                break;
             }
-            break;
             default:
                 System.out.println("Wrong input");
 
@@ -108,7 +111,7 @@ public class Main {
 
 
         sort(arr, n);
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.print("ID : " + arr[i].id);
             System.out.print("\tProfit : " + arr[i].profit);
             System.out.println("\tDeadline : " + arr[i].deadline);
@@ -120,7 +123,7 @@ public class Main {
         System.out.println("max : " + max);
         int countJobs = 0, jobProfit = 0;
         int[] result = new int[max+1];
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = arr[i].deadline; j > 0; j--) {
                 if (s.findslot(result, j)) {
                     result[j] = arr[i].id;
@@ -132,8 +135,9 @@ public class Main {
         }
         System.out.println("Jobs Done : ");
         for (int i = 1; i <= max; i++) {
-            if(result[i]==0) continue;
-            arr[result[i]-1].printdata();
+            System.out.println(result[i]);
+
+
         }
         System.out.println("Job : " + countJobs + "\t Profit : " + jobProfit);
 
