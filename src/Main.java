1e3ff4f64d732fabc3a03c8c227d37e78f1e59f2
rya23 <aaryathakur48@gmail.com>
2023-09-26 21:26:18 +0530

    Corrected Sorting

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-////Things to do
+//Things to do
 //Implement a better sorting algorithm
 //Exception Handling
 //Loops initialization numbering
@@ -47,11 +47,10 @@ class solve {
 public class Main {
 
     static void sort(Job[] arr, int n) {
-        System.out.println(arr[1].profit);
         int i, j;
         Job temp;
-        for (i = 1; i < n; i++) {
-            for (j = 1; j < n - i; j++) {
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n - i; j++) {
                 if (arr[j].profit < arr[j + 1].profit) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -87,21 +86,26 @@ public class Main {
                 break;
             }
             case 2: {
-//                n = 4;
-//                arr = new Job[n+1];
-//                arr[1] = new Job(1, 20, 4);
-//                arr[2] = new Job(2, 10, 1);
-//                arr[3] = new Job(3, 40, 2);
-//                arr[4] = new Job(4, 20, 2);
-
-                n = 6;
+                n = 4;
                 arr = new Job[n+1];
-                arr[1] = new Job(1, 30, 3);
-                arr[2] = new Job(2, 25, 1);
-                arr[3] = new Job(3, 50, 3);
-                arr[4] = new Job(4, 40, 2);
-                arr[5] = new Job(5, 10, 1);
-                arr[6] = new Job(6, 35, 4);
+                arr[1] = new Job(1, 20, 4);
+                arr[2] = new Job(2, 10, 1);
+                arr[3] = new Job(3, 40, 2);
+                arr[4] = new Job(4, 20, 2);
+
+//                n = 6;
+//                arr = new Job[n+1];
+//                arr[1] = new Job(1, 30, 3);
+//                arr[2] = new Job(2, 25, 1);
+//                arr[3] = new Job(3, 50, 3);
+//                arr[4] = new Job(4, 40, 2);
+//                arr[5] = new Job(5, 10, 1);
+//                arr[6] = new Job(6, 35, 4);
+                for (int i = 1; i <= n; i++) {
+                    arr[i].printdata();
+
+                }
+
                 break;
             }
             default:
@@ -109,24 +113,21 @@ public class Main {
 
         }
 
-
         sort(arr, n);
+        System.out.println("\n\n\nAfter Sorting");
         for (int i = 1; i <= n; i++) {
-            System.out.print("ID : " + arr[i].id);
-            System.out.print("\tProfit : " + arr[i].profit);
-            System.out.println("\tDeadline : " + arr[i].deadline);
+            arr[i].printdata();
 
         }
-
         solve s = new solve();
         int max = s.maxdeadline(arr, n);
-        System.out.println("max : " + max);
+        System.out.println("Max Deadline : " + max);
         int countJobs = 0, jobProfit = 0;
         int[] result = new int[max+1];
         for (int i = 1; i <= n; i++) {
             for (int j = arr[i].deadline; j > 0; j--) {
                 if (s.findslot(result, j)) {
-                    result[j] = arr[i].id;
+                    result[j] = i;
                     countJobs++;
                     jobProfit += arr[i].profit;
                     break;
@@ -135,7 +136,9 @@ public class Main {
         }
         System.out.println("Jobs Done : ");
         for (int i = 1; i <= max; i++) {
-            System.out.println(result[i]);
+            if(result[i]==0) continue;
+            int number = result[i];
+            arr[number].printdata();
 
 
         }
